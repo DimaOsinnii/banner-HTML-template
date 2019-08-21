13990ec66ec4643b3a7d5ebc9ca78b54f52b4e51
DimaOsinnii <ozerland@gmail.com>
2019-08-22 00:03:11 +0300

    created HTML tamplate and implemented banner animation

---
+++ app/scripts/index.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.btn');
+const banner = document.querySelector('.banner');
+let flag = true;
+
+function Show() {
+    banner.style.transform = "translateY(100%)";
+    window.open("https://www.youtube.com/", "Youtube");
+    window.focus();
+    flag = !flag;
+}
+
+if (flag) {
+    banner.onmouseup = Show;
+}
