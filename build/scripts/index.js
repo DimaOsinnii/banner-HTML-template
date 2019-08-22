b0fcdfd2ea684917a1a2438cd520e9ec2df9758e
DimaOsinnii <ozerland@gmail.com>
2019-08-22 17:32:13 +0300

    global update adde test animations and pictures

--- build/scripts/index.js
+++ build/scripts/index.js
@@ -1,2 +1,2 @@
-var button=document.querySelector(".btn"),banner=document.querySelector(".banner"),flag=!0;function Show(){banner.style.transform="translateY(100%)",window.open("https://www.youtube.com/","Youtube"),window.focus(),flag=!flag}flag&&(banner.onmouseup=Show);
+var button=document.querySelector(".btn"),banner=document.querySelector(".banner"),flag=!0;function Show(){banner.style.transform="translateY(100%)",window.open("https://www.youtube.com/","Youtube"),window.focus(),flag=!flag}flag&&(button.onmouseup=Show);
 //# sourceMappingURL=index.js.map
